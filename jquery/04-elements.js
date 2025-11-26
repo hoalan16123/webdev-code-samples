9c0d7641d26061589aad47b7fbba91c2bcabb4eb
Caterina Paun <caterina.paun@gmail.com>
2025-11-25 23:17:38 -0800

    Update: jquery elements exercise with png for the list-style

--- jquery/04-elements.js
+++ jquery/04-elements.js
@@ -1,23 +1,21 @@
 $(document).ready(function () {
   $('body')
     .css({ fontFamily: 'sans-serif' })
-    .append($('<h1>').text('jQuery 04 - Elements'))
+    .append($('<h1>').text('jQuery 04 - Creating Elements'))
     .append(
-      $('<main>')
-        .css({
-          marginTop: '10px',
-          padding: '10px',
-          backgroundColor: 'lightyellow',
-          lineHeight: '1.6em',
-        })
-        .append(
-          $('<ul>')
-            .append($('<li>').text('SpongeBob SquarePants'))
-            .append($('<li>').text('Patrick Star'))
-            .append($('<li>').text('Sandy Cheeks'))
-            .append($('<li>').text('Pearl Krabs'))
-            .append($('<li>').text('Mr Krabs'))
-            .append($('<li>').text('Squidward Tentacles'))
-        )
+      $('<main>').append(
+        $('<ul>')
+          .css({
+            listStyleImage: 'url(../_assets/spongebob.png)',
+            marginLeft: '40px',
+            textDecoration: 'underline',
+          })
+          .append($('<li>').text('SpongeBob SquarePants'))
+          .append($('<li>').text('Patrick Star'))
+          .append($('<li>').text('Sandy Cheeks'))
+          .append($('<li>').text('Pearl Krabs'))
+          .append($('<li>').text('Mr Krabs'))
+          .append($('<li>').text('Squidward Tentacles'))
+      )
     );
 });
